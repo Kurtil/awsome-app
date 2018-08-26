cc735381464209c406bc4ddb73249d4080a4000e
g.lagier <gaetan.lagier@hotmail.fr>
2018-08-26 20:24:29 +0200

    fix spelling mistake in index response

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello Wolrd from Awesome App !');
+  res.send('Hello World from Awesome App !');
 });
 
 module.exports = router;
