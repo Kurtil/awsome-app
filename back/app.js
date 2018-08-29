752bb5fcb4d0b5b58a1953665708e1ab341ceaa7
g.lagier <gaetan.lagier@hotmail.fr>
2018-08-29 20:16:41 +0200

    add headers in back to disabled errors in front (Access-Control-Allow-*)

--- back/app.js
+++ back/app.js
@@ -24,6 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
