de366ad2f983ad6fc234b7d767f7b85239102fbe
g.lagier <gaetan.lagier@hotmail.fr>
2018-09-02 11:56:12 +0200

    update back user controller -> from callback to promise async/await

--- back/controllers/users.controller.js
+++ back/controllers/users.controller.js
@@ -1,6 +1,10 @@
 const UserModel = require('../models/users.model')
 
-module.exports.user_create = function (req, res, next) {
+module.exports.user_create = async (req, res, next) => {
+    if (!req.body.firstName || !req.body.lastName) {
+        return res.status(400).json({ "error": "FirstName and lastName are mandatories!" });
+    }
+
     let user = new UserModel(
         {
             firstName: req.body.firstName,
@@ -9,63 +13,62 @@ module.exports.user_create = function (req, res, next) {
         }
     );
 
-    user.save(function (err, createdUser) {
-        if (err) {
-            return next(err);
-        }
-        res.send(`User created successfully with id : ${createdUser._id}`);
-    });
+    let savedUser;
+    try {
+        savedUser = await user.save();
+    } catch (err) {
+        return next(err);
+    }
+
+    res.status(200).json({ "success": `User created successfully with id : ${savedUser._id}` });
+
 };
 
-module.exports.user_read = function (req, res, next) {
-    UserModel.findById(req.params.id, function (err, user) {
-        if (err) {
-            return next(err);
-        } else if (!user) {
-            res.statusMessage = "This user does not exist!";
-            res.status(400).end();
-        } else {
-            res.send(user);
-        }
-    })
+module.exports.user_read = async (req, res, next) => {
+    let user;
+    try {
+        user = await UserModel.findById(req.params.id);
+    } catch (err) {
+        return next(err);
+    }
+
+    res.status(201).json(user);
 }
 
-module.exports.user_readall = function (req, res, next) {
-    UserModel.find(function (err, users) {
-        if (err) {
-            return next(err);
-        }
-        res.send(users);
-    })
+module.exports.user_readall = async (req, res, next) => {
+    let users;
+    try {
+        users = await UserModel.find();
+    } catch (err) {
+        return next(err)
+    }
+
+    res.send(users);
 }
 
-module.exports.user_update = function (req, res, next) {
+module.exports.user_update = async (req, res, next) => {
     let updateObject = {};
     if (req.body.firstName) updateObject.firstName = req.body.firstName;
     if (req.body.lastName) updateObject.lastName = req.body.lastName;
     if (req.body.inscriptionDate) updateObject.inscriptionDate = req.body.inscriptionDate;
 
-    UserModel.findByIdAndUpdate(req.params.id, {$set: updateObject}, function (err, updatedUser) {
-        if (err) {
-            return next(err);
-        } else if (!updatedUser) {
-            res.statusMessage = "This user does not exist!";
-            res.status(400).end();
-        } else {
-            res.send(`User with id ${updatedUser._id} udpated successfully!`);
-        }
-    });
+    let user;
+    try {
+        user = await UserModel.findByIdAndUpdate(req.params.id, { $set: updateObject });
+    } catch(err) {
+        return res.next(err);
+    }
+
+    res.status(200).json({"success" : `User with id ${user._id} udpated successfully!`});
 }
 
-module.exports.user_delete = function (req, res, next) {
-    UserModel.findByIdAndRemove(req.params.id, function (err, doc) {
-        if (err) {
-            return next(err);
-        } else if (!doc) {
-            res.statusMessage = "This user does not exist!";
-            res.status(400).end();
-        } else {
-            res.send(`User successfully deleted!`);
-        }
-    });
+module.exports.user_delete = async (req, res, next) => {
+    let deletedUser;
+    try {
+        deletedUser = await UserModel.findByIdAndRemove(req.params.id);
+    } catch (err) {
+        return next(err);
+    }
+
+    res.status(200).json({ "success": `user with firstname ${deletedUser.firstName} and lastname ${deletedUser.lastName} delete successfully!` });
 }
